baf0fdadb121485f5729be4aa71140359bf276ee
Shamoon Siddiqui <shamoon@develop.io>
2019-04-16 13:26:41 -0400

    Added batch norm

--- Q3.py
+++ Q3.py
@@ -32,22 +32,22 @@ input_img = Input(shape=(32, 32, 3))
 # Create Volumes for the Inception module
 volume_1 = Conv2D(64, (1, 1), padding='same', activation='relu')(input_img)
 volume_1 = Dropout(0.5)(volume_1)
-volume_1 = BatchNormalization(0.5)(volume_1)
+volume_1 = BatchNormalization()(volume_1)
 
 volume_2 = Conv2D(96, (1, 1), padding='same', activation='relu')(input_img)
 volume_2 = Conv2D(128, (3, 3), padding='same', activation='relu')(volume_2)
 volume_2 = Dropout(0.5)(volume_2)
-volume_2 = BatchNormalization(0.5)(volume_2)
+volume_2 = BatchNormalization()(volume_2)
 
 volume_3 = Conv2D(16, (1, 1), padding='same', activation='relu')(input_img)
 volume_3 = Conv2D(32, (5, 5), padding='same', activation='relu')(volume_3)
 volume_3 = Dropout(0.5)(volume_3)
-volume_3 = BatchNormalization(0.5)(volume_3)
+volume_3 = BatchNormalization()(volume_3)
 
 volume_4 = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(input_img)
 volume_4 = Conv2D(32, (1, 1), padding='same', activation='relu')(volume_4)
 volume_4 = Dropout(0.5)(volume_4)
-volume_4 = BatchNormalization(0.5)(volume_4)
+volume_4 = BatchNormalization()(volume_4)
 
 # Concatenate all volumes of the Inception module
 inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,

05968da59d23db45e0ff6dab0a764df3f7e61cb0
Shamoon Siddiqui <shamoon@develop.io>
2019-04-20 11:16:59 -0400

    Modified Q1

--- Q1.py
+++ Q1.py
@@ -66,10 +66,12 @@ Hint: You can use (https://keras.io/preprocessing/image/#imagedatagenerator)
 datagen = ImageDataGenerator(
     featurewise_center=True,
     featurewise_std_normalization=True,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    horizontal_flip=True)
+    rotation_range=90,
+    width_shift_range=0.5,
+    height_shift_range=0.5,
+    horizontal_flip=True,
+    vertical_flip=True
+    )
 
 
 history = model.fit_generator(datagen.flow(
